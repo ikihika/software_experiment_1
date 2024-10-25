8b3a740b21042136eb714d5226b5dab44e02c471
riosuke-kaziwara <ryousuke_113@icloud.com>
2024-10-25 17:08:27 +0900

    Add files via upload

--- interput.s
+++ interput.s
@@ -13,18 +13,18 @@
 INTERPUT:
 	move.l %d0, -(%sp) /*スタック退避*/
 	move.w #0x2700, %SR /*走行レベルを7に設定*/
-	cmpi.b #0, %d1
+	cmpi.l #0, %d1
 	bne    END_INTERPUT /*チャネルが０以外のとき何もせずに復帰*/
-	move.b #1, %d0      /*送信キューを選択*/
+	move.l #1, %d0      /*送信キューを選択*/
 	jsr    OUTQ         /*OUTQを実行*/
 	cmpi.b #0, %d0
 	beq    MASK         /*キューが空のときマスクを実行*/
-	move.w %d1, 0xFFF906 /*UTX1*にキューのデータを送信*/
-	ori.w  #0x0800, 0xFFF906/*上位8bitのヘッダ付与*/
+	addi.w  #0x0800, %d1/*上位8bitのヘッダ付与*/
+	move.w  %d1, UTX1   /*UTX1にデータ送信*/
 	bra    END_INTERPUT
 
 MASK:
-	andi.w #0xDFFF, 0xFFF900 /*送信割り込みの禁止*/
+	andi.w #0xFFFB, USTCNT1 /*送信割り込みの禁止*/
 
 
 END_INTERPUT:
