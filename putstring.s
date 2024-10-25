ec4ceaa21623322c681a30f7d1e0d4ddcd6b3cbb
riosuke-kaziwara <ryousuke_113@icloud.com>
2024-10-25 17:49:54 +0900

    Add files via upload

--- putstring.s
+++ putstring.s
@@ -18,22 +18,23 @@ PUTSTRING:
 	bne    END_PUTSTRING  /*チャネルが０以外のとき何もせずに復帰*/
 	move.l #0, (%a0)
 	move.l %d2, %a1
+	move.l #0, %d4       /*sz(%d4)*/
 	bra    PUT_LOOP
 
 
 PUT_LOOP:
 	cmpi.l #0, %d3
 	beq    SET_SIZE       /*sizeが０のときPUTSTRINGを終了*/
-	cmp.l  %d4, %d3       /*sz(%d4)*/
+	cmp.l  %d4, %d3      
 	beq    ANMASK         /*sz = sizeのときアンマスク*/
 	move.l #1, %d0        /*送信キューを指定*/
 	move.b (%a1), %d1     /*送信するデータを指定*/
 	jsr    INQ
 	cmpi.l #0, %d0     
 	beq    ANMASK         /*INQが失敗のとき*/
-	addai.b  #1,  %a1     /*次のバイトに移動*/
+	addi.l #1, %a1       /*次のバイトに移動*/
 	addi.l #1, %d4        /*szをインクリメント*/
-	bra    LOOP1
+	bra    PUT_LOOP
 
 
 ANMASK:
