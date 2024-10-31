a621c1e1c743299b25a691b3519db61de08e511c
Harutasan <e1010c@icloud.com>
2024-10-31 16:41:27 +0900

    Update echo.s

--- echo.s
+++ echo.s
@@ -1,4 +1,3 @@
-
 ***************************************************************
 ** 各種レジスタ定義
 ***************************************************************
@@ -53,10 +52,12 @@
 SYS_STK:
 	.ds.b 0x4000 /* システムスタック領域 */
 	.even
+SYS_STK_TOP: /* システムスタック領域の最後尾 */
+
 task_p: 
 	.ds.l 0x1 /*大域変数task_pの設定*/
 	.even
-SYS_STK_TOP: /* システムスタック領域の最後尾 */
+
 
 ***************************************************************
 ** 初期化
@@ -90,7 +91,11 @@ boot:
 	*****************
 	move.w #0x0004, TCTL1 /* restart, 割り込み不可, */
 				/* システムクロックの 1/16 を単位として計時， */
-				/* タイマ使用停止 */
+	/* タイマ使用停止 */
+
+
+
+	
 	****************
 	** 割り込みベクタの設定
 	****************
@@ -99,7 +104,7 @@ boot:
 	****************
 	** 送受信 (UART1) 関係の変更
 	****************
-	move.w #0xe100, USTCNT1 /* 送受信可能, パリティなし, 1 stop, 8 bit, */
+	move.w #0xe10c, USTCNT1 /* 送受信可能, パリティなし, 1 stop, 8 bit, */
 				/* 受信割り込み許可、送信割り込み許可 */
 
 	****************
@@ -111,6 +116,7 @@ boot:
 	** 走行レベルの設定
 	****************
 	move.w #0x2000, %SR /* スーパバイザモード, 割り込み全許可 */
+	jsr Init_Q               /* キューの初期化 */	
 
 
 ****************
@@ -133,6 +139,203 @@ boot:
 	
 	bra MAIN
 
+
+/* キューの初期化 */
+Init_Q:
+	move.l %a0, -(%sp)
+	lea.l   BF0_START, %a0
+	move.l  %a0, PUT_PTR0
+	move.l  %a0, GET_PTR0
+	move.b  #0xff, PUT_FLG0
+	move.b  #0x00, GET_FLG0
+
+	lea.l   BF1_START, %a0
+	move.l  %a0, PUT_PTR1
+	move.l  %a0, GET_PTR1
+	move.b  #0xff, PUT_FLG1
+	move.b  #0x00, GET_FLG1
+	move.l (%sp)+,%a0
+	rts
+
+/* キュー番号に応じたポインタ・フラグ・バッファを設定 */
+SelectQueue:
+	move.l  %a6, -(%sp)
+	cmpi.l   #0, %d0
+	beq     UseQueue0
+	cmpi.l   #1, %d0
+	beq     UseQueue1
+
+UseQueue0:
+	move.l  PUT_PTR0, PUT_PTR
+	move.l  GET_PTR0, GET_PTR
+	move.b  PUT_FLG0, PUT_FLG
+	move.b  GET_FLG0, GET_FLG
+	lea.l   BF0_START, %a6
+	move.l  %a6, BF_START
+	lea.l   BF0_END, %a6
+	move.l  %a6, BF_END
+	move.l  (%sp)+, %a6
+	rts
+
+UseQueue1:
+	move.l  PUT_PTR1, PUT_PTR
+	move.l  GET_PTR1, GET_PTR
+	move.b  PUT_FLG1, PUT_FLG
+	move.b  GET_FLG1, GET_FLG
+	lea.l   BF1_START, %a6
+	move.l  %a6, BF_START
+	lea.l   BF1_END, %a6
+	move.l  %a6, BF_END
+	move.l  (%sp)+, %a6
+	rts
+
+/* キュー番号に応じたポインタ・フラグ・バッファを選択して更新する */
+UpdateQueuePointers:
+	cmpi.l   #0, %d0
+	beq     UpdateQueue0
+	cmpi.l   #1, %d0
+	beq     UpdateQueue1
+
+UpdateQueue0:
+	move.l  PUT_PTR, PUT_PTR0
+	move.l  GET_PTR, GET_PTR0
+	move.b  PUT_FLG, PUT_FLG0
+	move.b  GET_FLG, GET_FLG0
+	rts
+
+UpdateQueue1:
+	move.l  PUT_PTR, PUT_PTR1
+	move.l  GET_PTR, GET_PTR1
+	move.b  PUT_FLG, PUT_FLG1
+	move.b  GET_FLG, GET_FLG1
+	rts
+
+*********************************************************
+**InQ(no, data)
+**入力:キュー番号no(%d0.L), 書き込む8bitデータdata(%d1.B)
+**出力:失敗0/成功1(%d0.L)
+**********************************************************
+INQ:
+	move.w %sr, -(%sp)  /*走行レベルを退避*/
+	move.w #0x2700, %SR /*走行レベルを7に設定*/
+	jsr SelectQueue
+	jsr PUT_BUF
+	move.w (%sp)+, %sr /*走行レベルの回復*/
+	rts
+
+
+PUT_BUF:
+	movem.l %d2/%a1-%a3, -(%sp) /* スタック退避 */
+	move.b  PUT_FLG, %d2
+	cmp.b   #0x00, %d2
+	beq     PUT_BUF_Fail /* キューが満杯のとき */
+	movea.l PUT_PTR, %a1
+	move.b  %d1, (%a1)+
+	move.l   BF_END, %a3
+	cmpa.l  %a3, %a1
+	bls     PUT_BUF_STEP1
+	move.l   BF_START, %a2
+	movea.l %a2, %a1
+
+PUT_BUF_STEP1:
+	move.l  %a1, PUT_PTR
+	cmpa.l  GET_PTR, %a1
+	bne     PUT_BUF_STEP2
+	move.b  #0x00, PUT_FLG
+
+PUT_BUF_STEP2:
+	move.b  #0xff, GET_FLG
+	jsr     UpdateQueuePointers
+	move.l  #1, %d0  /* 成功したときd0を1にセット */
+	bra PUT_BUF_Finish
+
+PUT_BUF_Fail:
+	move.l  #0, %d0 /* 失敗したときd0を0にセット */
+
+PUT_BUF_Finish:
+	movem.l (%sp)+, %d2/%a1-%a3
+	rts
+
+
+***********************************************************
+**OUTQ(no,data)
+**入力:キュー番号no(%d0.L)
+**出力:失敗0/成功1(%d0.L), 取り出した8bitデータdata(%d1.B)
+**********************************************************
+OUTQ:
+	move.w  %sr, -(%sp) /*走行レベルの退避*/
+	move.w  #0x2700, %SR /*走行レベルを7に設定*/
+	jsr SelectQueue
+	jsr GET_BUF
+	move.w  (%sp)+, %sr /*走行レベルの回復*/
+	rts
+
+
+GET_BUF:
+	movem.l %d2/%a1-%a3, -(%sp) /* スタック退避 */
+	move.b  GET_FLG, %d2
+	cmpi.b   #0x00, %d2
+	beq     GET_BUF_Fail /* キューが空のとき */
+	movea.l GET_PTR, %a1
+	move.b  (%a1), %d1
+	move.l   BF_END, %a3
+	cmpa.l   %a3, %a1
+	bls      GET_BUF_INCREMENT
+	move.l   BF_START, %a2
+	movea.l  %a2, %a1
+	bra GET_BUF_STEP1
+
+GET_BUF_INCREMENT:
+	addq.l #1,%a1
+	bra GET_BUF_STEP1
+
+GET_BUF_STEP1:
+	move.l  %a1, GET_PTR
+	cmpa.l  PUT_PTR, %a1
+	bne     GET_BUF_STEP2
+	move.b  #0x00, GET_FLG
+
+GET_BUF_STEP2:
+	move.b  #0xff, PUT_FLG
+	jsr     UpdateQueuePointers
+	move.l  #1, %d0 /* 成功したときd0を1にセット */
+	bra     GET_BUF_Finish 
+
+GET_BUF_Fail:
+	move.l  #0, %d0 /* 失敗したときd0を0にセット */
+GET_BUF_Finish:
+	movem.l (%sp)+, %d2/%a1-%a3
+	rts
+
+.section .data
+.even
+.equ  B_SIZE,  256
+	
+/*受信用のキュー*/
+BF0_START:  .ds.b  B_SIZE-1
+BF0_END:    .ds.b  1
+PUT_PTR0:   .ds.l  1
+GET_PTR0:   .ds.l  1
+PUT_FLG0:   .ds.b  1
+GET_FLG0:   .ds.b  1
+
+/*送信用のキュー*/
+BF1_START:  .ds.b  B_SIZE-1
+BF1_END:    .ds.b  1
+PUT_PTR1:   .ds.l  1
+GET_PTR1:   .ds.l  1
+PUT_FLG1:   .ds.b  1
+GET_FLG1:   .ds.b  1
+
+PUT_PTR:    .ds.l  1  /* 共通ポインタ */
+GET_PTR:    .ds.l  1  /* 共通ポインタ */
+PUT_FLG:    .ds.b  1  /* 共通フラグ */
+GET_FLG:    .ds.b  1  /* 共通フラグ */
+BF_START:   .ds.l  1  /* バッファ開始ポインタ */
+BF_END:     .ds.l  1  /* バッファ終了ポインタ */
+
+/*step6テスト用データ*/
+WORK: .ds.b 256
 
 
 ***************************************************************
@@ -202,24 +405,42 @@ TTEND:
 	move.b #'6',LED2
 	movem.l (%SP)+,%d0-%d7/%a0-%a6
 	rts
-
+	
 .section .text
 .even
 uart1_interrupt:
-	movem.l %d0-%d7/%a0-%a6, -(%sp)   /* レジスタの退避 */
-	move.w  UTX1, %d0                 /* UTX1の内容を一時的に保存 */
-	andi.w  #0x8000, %d0              /* 15ビット目のみを抽出 */
-	cmpi.w  #0x8000, %d0              /* 15ビット目が0→送信割り込み */
-	bne     end_interrupt             /* 送信割り込みでなければ終了 */
+	movem.l %d0-%d5/%a0-%a6, -(%sp)   /* レジスタの退避 */
+
+	
+	
+
+check_send:
+	move.w  UTX1, %d0                 /* UTX1の内容をd0に一時的に保存 */
+	andi.w #0x8000,%d0                  /* 送信レジスタの15ビット目をチェック */
+	cmpi.w #0x8000,%d0
+	bne    check_receive             /* 送信割り込みでなければ受信チェックへ */
 
 	/* 送信割り込みの場合 */
+
 	moveq.l #0, %d1                   /* ch = 0 を設定 */
 	jsr     INTERPUT                  /* INTERPUTを呼び出し */
 
-end_interrupt:
-	movem.l (%sp)+, %d0-%d7/%a0-%a6   /* レジスタの復帰 */
-	rte                               /* 割り込みからの復帰 */
+check_receive:
+	move.w  URX1, %d3                 /* URX1の内容をd3に一時的に保存 */
+	move.b  %d3, %d2                  /* d3の下位8bit(データ部分)をd2にコピー */
+	andi.w #0x2000,%d3		  /* 受信レジスタの13ビット目をチェック */
+	cmpi.w #0x2000,%d3
+	bne     end_interrupt             /* 受信割り込みでなければ終了 */
 	
+
+	/* 受信割り込みの場合 */
+	moveq.l #0, %d1 /* ch = 0 を設定 */
+	jsr     INTERGET                  /* INTERGETを呼び出し */
+
+end_interrupt:
+	movem.l (%sp)+, %d0-%d5/%a0-%a6   /* レジスタの復帰 */
+	rte                               /* 割り込みからの復帰 */
+
 .section .text
 .even
 timer_interrupt:
@@ -312,46 +533,47 @@ CALL_Finish:
 	movem.l (%sp)+,%a0
 	rte	
 
-************
-*interput
-**********
-.section .text
-
-**********************************
+	
+*********************************
 **INTERPUT(ch)
 **入力:チャネルch(%d1.L)
 **戻り値:なし
 *********************************	
-INTERPUT:
-	move.l %d0, -(%sp)      /*スタック退避*/
-	move.w #0x2700, %SR     /*走行レベルを7に設定*/
-	
+INTERPUT:	
+	move.b #'4',LED4
+	move.l %d0, -(%sp) /*スタック退避*/
+	move.w %sr, -(%sp)
+	move.w #0x2700, %SR /*走行レベルを7に設定*/
 	cmpi.l #0, %d1
-	bne    END_INTERPUT     /*チャネルが０以外のとき何もせずに復帰*/
-	
-	move.l #1, %d0          /*送信キューを選択*/
-	jsr    OUTQ             /*OUTQを実行*/
-	cmpi.b #0, %d0
-	beq    MASK             /*キューが空のときマスクを実行*/
-	
-	addi.w  #0x0800, %d1    /*上位8bitのヘッダ付与*/
-	move.w  %d1, UTX1       /*UTX1にデータ送信*/
+	bne    END_INTERPUT /*チャネルが０以外のとき何もせずに復帰*/
+	move.l #1, %d0      /*送信キューを選択*/
+	jsr    OUTQ         /*OUTQを実行*/
+	move.b %d0,LED2
+	cmpi.l #0, %d0
+	beq    MASK         /*キューが空のときマスクを実行*/
+	andi.w #0x00ff,%d1
+	addi.w #0x800,%d1
+	move.w  %d1, UTX1/*上位8bitのヘッダ付与*/
 	bra    END_INTERPUT
 
 MASK:
-	andi.w #0xFFFB, USTCNT1 /*送信割り込みの禁止*/
-
+	addq.b #1,%d6
+	andi.b #0x0f,%d6
+	move.b %d6,LED0
+	andi.w #0xfffb, USTCNT1 /*送信割り込みの禁止*/
 
 END_INTERPUT:
-	move.l (%sp)+, %d0      /*スタック回復*/
+	move.w (%sp)+, %sr
+	move.l (%sp)+, %d0
 	rts
-	
-************
-*putstring
-**********
+
+
+*************************************
+**作成者：梶原, 松木
+**コーディング：梶原, チェック：松木
+************************************
 .section .text
-
-
+.even
 ************************************************************************************
 **PUTSTRING(ch, p, size)
 **入力:チャネルch(%d0.l), 読み込み先の先頭アドレスp(%d2.l), 送信するデータ数size(%d3.l)
@@ -359,10 +581,12 @@ END_INTERPUT:
 *************************************************************************************
 
 PUTSTRING:
-	movem.l %d4/%a1, -(%sp)
+	move.b #'3',LED5         /* LED5 を光らせる */
+	movem.l %d4/%a0-%a1, -(%sp)
 	cmpi.l #0, %d0
 	bne    END_PUTSTRING  /*チャネルが０以外のとき何もせずに復帰*/
-	move.l %d2, %a1      /*読み込み先の先頭アドレスを格納*/
+	move.l #0, (%a0)
+	move.l %d2, %a1
 	move.l #0, %d4       /*sz(%d4)*/
 	bra    PUT_LOOP
 
@@ -370,18 +594,15 @@ PUTSTRING:
 PUT_LOOP:
 	cmpi.l #0, %d3
 	beq    SET_SIZE       /*sizeが０のときPUTSTRINGを終了*/
-	
 	cmp.l  %d4, %d3      
 	beq    ANMASK         /*sz = sizeのときアンマスク*/
-	
 	move.l #1, %d0        /*送信キューを指定*/
 	move.b (%a1), %d1     /*送信するデータを指定*/
 	jsr    INQ
 	cmpi.l #0, %d0     
 	beq    ANMASK         /*INQが失敗のとき*/
-	
 	addq.l #1, %a1       /*次のバイトに移動*/
-	addq.l #1, %d4        /*szをインクリメント*/
+	addi.l #1, %d4        /*szをインクリメント*/
 	bra    PUT_LOOP
 
 
@@ -393,14 +614,9 @@ SET_SIZE:
 	move.l %d4, %d0 /*戻り値の設定*/
 	
 END_PUTSTRING:
-	movem.l (%sp)+, %d4/%a1 /*レジスタ復帰*/
+	movem.l (%sp)+, %d4/%a0-%a1
 	rts
 
-	
-************
-*interget
-**********
-	.section .text
 *************************************************
 **受信データを受信キューに格納する
 **入力：チャネルch(%d1.l)
@@ -409,31 +625,28 @@ END_PUTSTRING:
 *************************************************
 
 INTERGET:
-        movem.l %d0,-(%sp)	/* レジスタ退避 */
+	move.b #'5',LED3
         cmpi.l  #0,%d1		/* ch≠0ならなにもせず復帰 */
         bne     END_INTERGET
-        move.l  %d2,%d1		/* INQの入力d1に受信データを格納 */
+        move.b  %d2,%d1		/* INQの入力d1に受信データを格納 */
 	move.l	#0,%d0		/* キュー番号を0にする */
         jsr     INQ
 
 END_INTERGET:
-        movem.l (%sp)+,%d0	/* レジスタの回復 */
         rts
-	
-************
-*getstring
-**********
-	.section .text
+
+
 ***********************************************
 **チャネルchの受信キューからsizeバイトのデータを取り出しp番地以降にコピーする
 **入力：チャネルch(%d1.L)
 **	書き込み先の先頭アドレスp(%d2.L)
 **	取り出すデータ数size(%d3.L)
 **戻り値：じっさいに取り出したデータ数sz(%d0.L)
-	***********************************************
+***********************************************
 
 
 GETSTRING:
+	move.b #'2',LED6         /* LED6 を光らせる */
 	movem.l	%d4/%a1,-(%sp)	/* レジスタの退避 */
 	cmpi.l	#0,%d1
 	bne	END_GETSTRING	/* チャネルが0以外ならEND_GETSTRINGへ */
@@ -446,7 +659,7 @@ GET_LOOP:
 	jsr	OUTQ		/* 出力：失敗0/成功1(%d0)、8bitのdata(%d1) */
 	cmpi.l	#0,%d0
 	beq	Input		/* OUTQの復帰値が0ならInputへ */
-	move.l	%d1,(%a1)	/* i番地にdata(OUTQの出力値)をcopy */
+	move.b	%d1,(%a1)	/* i番地にdata(OUTQの出力値)をcopy */
 	addi.l	#1,%d4		/* sz++ */
 	addq.l	#1,%a1		/* i++ */
 	bra	GET_LOOP
@@ -457,234 +670,3 @@ Input:
 END_GETSTRING:
 	movem.l	(%sp)+,%d4/%a1	/* レジスタの回復 */
 	rts
-
-******
-*queue
-******
-.section .text
-
-start:
-    jsr Init_Q
-    move.l  #LENGTH, %d4
-    move.l  #LENGTH, %d5
-    lea.l   Data_to_Que, %a0 
-    lea.l   InQ_result, %a1  
-    lea.l   Output,   %a2
-    lea.l   OutQ_result, %a3
-
-    /* INQを繰り返す */
-LOOP1:
-    subq.w   #1, %d4
-    bcs      LOOP2
-    move.b   (%a0)+, %d1   /*入力するデータ*/
-    move.w   #1, %d0       /* キュー番号 0 */
-    jsr      INQ          /* サブルーチン呼び出し */
-    move.l   %d0, (%a1)+
-    bra      LOOP1
-
-
-    /* OUTQを繰り返す */
-LOOP2:
-    subq.w   #1, %d5
-    bcs      End_of_program
-    move.w   #1, %d0      /* キュー番号 0 */
-    jsr      OUTQ         /* サブルーチン呼び出し */
-    move.l   %d0, (%a3)+
-    cmp.b    #0, %d0
-    beq      LOOP2
-    move.b   %d1, (%a2)+
-    bra      LOOP2
-
-End_of_program:
-    stop     #0x2700
-
-/* キューの初期化 */
-Init_Q:
-    lea.l   BF0_START, %a0
-    move.l  %a0, PUT_PTR0
-    move.l  %a0, GET_PTR0
-    move.b  #0xff, PUT_FLG0
-    move.b  #0x00, GET_FLG0
-
-    lea.l   BF1_START, %a0
-    move.l  %a0, PUT_PTR1
-    move.l  %a0, GET_PTR1
-    move.b  #0xff, PUT_FLG1
-    move.b  #0x00, GET_FLG1
-    rts
-
-/* キュー番号に応じたポインタ・フラグ・バッファを設定 */
-SelectQueue:
-    cmp.w   #0, %d0
-    beq     UseQueue0
-    cmp.w   #1, %d0
-    beq     UseQueue1
-
-UseQueue0:
-    move.l  PUT_PTR0, PUT_PTR
-    move.l  GET_PTR0, GET_PTR
-    move.b  PUT_FLG0, PUT_FLG
-    move.b  GET_FLG0, GET_FLG
-    lea.l   BF0_START, %a6
-    move.l  %a6, BF_START
-    lea.l   BF0_END, %a6
-    move.l  %a6, BF_END
-    rts
-
-UseQueue1:
-    move.l  PUT_PTR1, PUT_PTR
-    move.l  GET_PTR1, GET_PTR
-    move.b  PUT_FLG1, PUT_FLG
-    move.b  GET_FLG1, GET_FLG
-    lea.l   BF1_START, %a6
-    move.l  %a6, BF_START
-    lea.l   BF1_END, %a6
-    move.l  %a6, BF_END
-    rts
-
-/* キュー番号に応じたポインタ・フラグ・バッファを選択して更新する */
-UpdateQueuePointers:
-    cmp.w   #0, %d0
-    beq     UpdateQueue0
-    cmp.w   #1, %d0
-    beq     UpdateQueue1
-
-UpdateQueue0:
-    move.l  PUT_PTR, PUT_PTR0
-    move.l  GET_PTR, GET_PTR0
-    move.b  PUT_FLG, PUT_FLG0
-    move.b  GET_FLG, GET_FLG0
-    rts
-
-UpdateQueue1:
-    move.l  PUT_PTR, PUT_PTR1
-    move.l  GET_PTR, GET_PTR1
-    move.b  PUT_FLG, PUT_FLG1
-    move.b  GET_FLG, GET_FLG1
-    rts
-
-*********************************************************
-**InQ(no, data)
-**入力:キュー番号no(%d0.L), 書き込む8bitデータdata(%d1.B)
-**出力:失敗0/成功1(%d0.L)
-**********************************************************
-INQ:
-    move.w %sr, -(%sp)  /*走行レベルを退避*/
-    move.w #0x2700, %SR /*走行レベルを7に設定*/
-    jsr SelectQueue
-    jsr PUT_BUF
-    move.w (%sp)+, %sr /*走行レベルの回復*/
-    rts
-
-
-PUT_BUF:
-    movem.l %d2/%a1-%a3, -(%sp) /* スタック退避 */
-    move.b  PUT_FLG, %d2
-    cmp.b   #0x00, %d2
-    beq     PUT_BUF_Fail /* キューが満杯のとき */
-    movea.l PUT_PTR, %a1
-    move.b  %d1, (%a1)+
-    move.l   BF_END, %a3
-    cmpa.l  %a3, %a1
-    bls     PUT_BUF_STEP1
-    move.l   BF_START, %a2
-    movea.l %a2, %a1
-
-PUT_BUF_STEP1:
-    move.l  %a1, PUT_PTR
-    cmpa.l  GET_PTR, %a1
-    bne     PUT_BUF_STEP2
-    move.b  #0x00, PUT_FLG
-
-PUT_BUF_STEP2:
-    move.b  #0xff, GET_FLG
-    jsr     UpdateQueuePointers
-    move.b  #1, %d0  /* 成功したときd0を1にセット */
-    bra PUT_BUF_Finish
-
-PUT_BUF_Fail:
-    move.b  #0, %d0 /* 失敗したときd0を0にセット */
-
-PUT_BUF_Finish:
-    movem.l (%sp)+, %a3-%a1/%d2
-    rts
-
-
-***********************************************************
-**OUTQ(no,data)
-**入力:キュー番号no(%d0.L)
-**出力:失敗0/成功1(%d0.L), 取り出した8bitデータdata(%d1.B)
-**********************************************************
-OUTQ:
-    move.w  %sr, -(%sp) /*走行レベルの退避*/
-    move.w  #0x2700, %SR /*走行レベルを7に設定*/
-    jsr SelectQueue
-    jsr GET_BUF
-    move.w  (%sp)+, %sr /*走行レベルの回復*/
-    rts
-
-
-GET_BUF:
-    movem.l %d2/%a1-%a3, -(%sp) /* スタック退避 */
-    move.b  GET_FLG, %d2
-    cmp.b   #0x00, %d2
-    beq     GET_BUF_Fail /* キューが空のとき */
-    movea.l GET_PTR, %a1
-    move.b  (%a1)+, %d1
-    move.l   BF_END, %a3
-    cmpa.l   %a3, %a1
-    bls      GET_BUF_STEP1
-    move.l   BF_START, %a2
-    movea.l  %a2, %a1
-
-GET_BUF_STEP1:
-    move.l  %a1, GET_PTR
-    cmpa.l  PUT_PTR, %a1
-    bne     GET_BUF_STEP2
-    move.b  #0x00, GET_FLG
-
-GET_BUF_STEP2:
-    move.b  #0xff, PUT_FLG
-    jsr     UpdateQueuePointers
-    move.b  #1, %d0 /* 成功したときd0を1にセット */
-    bra     GET_BUF_Finish 
-
-GET_BUF_Fail:
-    move.b  #0, %d0 /* 失敗したときd0を0にセット */
-
-GET_BUF_Finish:
-    movem.l (%sp)+, %a3-%a1/%d2
-    rts
-
-.section .data
-    .equ  B_SIZE,  10
-/*受信用のキュー*/
-BF0_START:  .ds.b  B_SIZE-1
-BF0_END:    .ds.b  1
-PUT_PTR0:   .ds.l  1
-GET_PTR0:   .ds.l  1
-PUT_FLG0:   .ds.b  1
-GET_FLG0:   .ds.b  1
-
-/*送信用のキュー*/
-BF1_START:  .ds.b  B_SIZE-1
-BF1_END:    .ds.b  1
-PUT_PTR1:   .ds.l  1
-GET_PTR1:   .ds.l  1
-PUT_FLG1:   .ds.b  1
-GET_FLG1:   .ds.b  1
-
-PUT_PTR:    .ds.l  1  /* 共通ポインタ */
-GET_PTR:    .ds.l  1  /* 共通ポインタ */
-PUT_FLG:    .ds.b  1  /* 共通フラグ */
-GET_FLG:    .ds.b  1  /* 共通フラグ */
-BF_START:   .ds.l  1  /* バッファ開始ポインタ */
-BF_END:     .ds.l  1  /* バッファ終了ポインタ */
-
-
-.equ   LENGTH,   256 /*キューに入れるデータの個数*/
-Data_to_Que:  .ascii   "aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbccccccccccccccccddddddddddddddddeeeeeeeeeeeeeeeeffffffffffffffffgggggggggggggggghhhhhhhhhhhhhhhhiiiiiiiiiiiiiiiijjjjjjjjjjjjjjjjkkkkkkkkkkkkkkkkllllllllllllllllmmmmmmmmmmmmmmmmnnnnnnnnnnnnnnnnoooooooooooooooopppppppppppppppp"/*INQで読み込むためのデータ領域*/
-Output:       .ds.b  B_SIZE+2 /*OUT_Qの出力先*/
-InQ_result:   .ds.l  10  /*INQの戻り値(0 or 1)*/
-OutQ_result:  .ds.l  10  /*OUTQの戻り値(0 or 1)*/
-.end	
